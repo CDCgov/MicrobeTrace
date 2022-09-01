04ef31f3f3eedd1b8a5fbb5ee8133de7b74add0a
Evan Moscoso <qwu3@cdc.gov>
2022-09-01 09:17:38 -0400

    Revert "Removed titleization"
    This reverts commit cad7e19a970d3493d3dc4ede5cf30215d6f87e4a.

--- src/common.js
+++ src/common.js
@@ -1364,8 +1364,6 @@
     if (small == "geo map") return "Map";
     if (small == "timeline") return "Epi Curve";
     if (small == "nn") return "Nearest Neighbor";
-    return title;
-    // TODO add back titlize if required
     return small.replace(/(?:^|\s|-)\S/g, c => c.toUpperCase());
   };
 
