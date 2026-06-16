50c08a84d6fc326edad330fe006819ff93c741bb
mossy426-cdc <qwu3@cdc.gov>
2026-06-16 13:21:01 -0400

    Suppress recoverable runtime warning banners

--- src/environments/version.prod.ts
+++ src/environments/version.prod.ts
@@ -1,3 +1,3 @@
 export const version = '2.1.0';
-export const buildDate = '2026-04-14T17:49:17.042Z';
-export const commitHash = '3e9a9b15';
+export const buildDate = '2026-06-16T16:50:01.338Z';
+export const commitHash = '373bc483';
