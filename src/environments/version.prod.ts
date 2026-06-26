a32cd779841e7119484a84d2c7f6250e582ec93a
mossy426-cdc <qwu3@cdc.gov>
2026-06-26 11:15:37 -0400

    Update production version metadata

--- src/environments/version.prod.ts
+++ src/environments/version.prod.ts
@@ -1,3 +1,3 @@
 export const version = '2.2.0';
-export const buildDate = '2026-06-25T18:38:57.982Z';
-export const commitHash = '65f69165';
+export const buildDate = '2026-06-26T14:21:19.308Z';
+export const commitHash = 'e3ea10ef';
