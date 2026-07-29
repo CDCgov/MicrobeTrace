26d010f36f9cb5aa524fe926ab95e0b8b8875387
Guppy L. Stott <stott.garrick@gmail.com>
2026-07-29 10:21:30 -0400

    Reapply "Merge branch 'codex/network-statistics-updates' into dev"
    This reverts commit c649b4ed2814313e1c7dc64c9b53e00839c8013b.

--- src/app/filesComponent/files-plugin.component.ts
+++ src/app/filesComponent/files-plugin.component.ts
@@ -47,6 +47,7 @@ export class FilesComponent extends BaseComponentDirective implements OnInit {
     'Epi Curve',
     'Sankey',
     'Table',
+    'Network Statistics',
     'Crosstab',
     'Map',
     'Bubble',
